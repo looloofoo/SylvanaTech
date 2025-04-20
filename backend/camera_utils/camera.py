d9bdd08175727928cbe3e63c13f99b0afa823976
Luis Bin <binluis@gmail.com>
2025-04-20 18:33:07 +0200

    Making changes

--- backend/camera_utils/camera.py
+++ backend/camera_utils/camera.py
@@ -16,103 +16,88 @@ class CameraManager:
         self.camera = None
 
     def initialize(self):
-        # Starts camera 
+        """
+        Initialize camera
+        """ 
         self.camera = cv2.VideoCapture(self.camera_id)
 
         self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, self.resolution[0])
-        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, self.resolution[0])
+        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, self.resolution[1])
 
         if not self.camera.isOpened():
             raise RuntimeError("Failed to open camera. Check if it isn't connected and not in use by another app.")
-    
-        return RuntimeError("Failed to open camera. Check if it connected or being used by another application")
+ 
+        return self.camera.isOpened():
 
     def capture_frame(self):
-
+        """
+        Capture a single frame from the cameras
+        """
         if self.camera is None or not self.camera.isOpened():
-            raise RuntimeError("Camera is not initialized. Call initializer() first.")
-        
-        ret, frame = self.camera.read()
+            self.initialize()
 
+        ret, frame = self.camera.read()
+        
         if not ret:
             raise RuntimeError("Failed to capture frame from camera.")
         
         return frame
     
+    def save_frame(self, frame, output_dir, filename=None):
+        """
+        Save a frame to the specific directory
+        """
+        os.makedirs(output_dir, exist_ok=True)
+
+        if filename is None:
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f"image_{timestamp}.jpg"
+
+        if not filename.lower().endswith(('.jpg','.jpeg')):
+            filename += '.jpg'
+
+
+        file_path = os.path.join(output_dir, filename)
+        success = cv2.imwrite(file_path, frame)
+
+        if not success: 
+            raise RuntimeError(f"Failed to save image to {file_path}")
+
+        return file_path
+
     def capture_and_save(self, output_dir, filename=None):
         """
         This function captures a single frame and saves is to a directory
 
         Note: See if this part can sent to server and not saved locallly
         """
-
-        os.makedirs(output_dir, exist_ok=True)
-
-        frame = self.capture_frame()
-
-        if filename is None:
-            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-            filename = f"plant_{timestamp}.jpg"
-
-        image_path = os.path.join(output_dir, filename)
-        cv2.imwrite(image_path, frame)
-
-        return image_path, frame 
-    
-    def start_timelapse(self, output_dir, interval_seconds=3600, max_frames=None, display=True):
-        """
-        Note: The argument max frame should be set before runnig to assure we have enoght space locally"
-        Also: Display argument set to true can be changed to false if running in headless mode.     
-        """
         
-        os.makedirs(output_dir, exist_ok=True)
-
-        if self.camera is None:
-            self.initialize()
-
-        frame_count = 0
-        captured_paths = []
-
-        print(f"Starting timelapse. Images will be saved to {output_dir}")
-        print(f"Press 'q' to stop caturing")
-
-        try: 
-            while True: 
-                if max_frames is not None and frame_count >= max_frames:
-                    break 
-            
-            image_path, frame = self.capture_and_save(output_dir)
-            captured_paths.append(image_path)
-            frame_count += 1
-
-        except KeyboardInterrupt:
-            print("Timelapse stopped by user")
-        finally:
-            if display:
-                cv2.destroyAllWindows()
+        frame = self.capture_frame()
+        return self.save_frame(frame, output_dir, filename)
 
     def release(self):
+        """
+        Release the camera resources
+        """
         if self.camera is not None and self.camera.isOpened():
             self.camera.release()
             self.camera = None
 
-    def list_available_cameras(max_cameras=10):
-        """
-        Creating this since it could be useful in the future if more cameras are added to the rig.
-        """
-        available_cameras = {}
+def list_available_cameras(max_cameras=10):
+    """
+    Creating this since it could be useful in the future if more cameras are added to the rig.
+    """
+    available_cameras = {}
 
-        for camera_id in range(max_cameras):
-            camera = cv2.VideoCapture(camera_id)
-            if camera.isOpened():
-                ret, _ = camera.read()
-                if ret:
-                    width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
-                    heigth = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                    available_cameras[camera_id] = (width, heigth)
-                camera.release()
+    for camera_id in range(max_cameras):
+        camera = cv2.VideoCapture(camera_id)
+        if camera.isOpened():
+            ret, _ = camera.read()
+            if ret:
+                width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+                heigth = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                available_cameras[camera_id] = (width, heigth)
+            camera.release()
         
-        return available_cameras         
+    return available_cameras         
 
-
-                    
